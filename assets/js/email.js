db8998814d9cbf994f25ce45be723dda5541c9a3
Naveen <130822399+Naveen-767@users.noreply.github.com>
2024-03-13 15:21:04 +0530

    Add files via upload

---
+++ assets/js/email.js
@@ -0,0 +1,6 @@
+
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
